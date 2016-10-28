1d7d1a729db47e727b3ea6bb9ce951e2f192b3b3
Shrikshel <devshrikshel@gmail.com>
2016-10-28 12:41:21 +0530

    Added Success Form

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,5 +6,17 @@
  */
 
 module.exports = {
+  login:function (req,res,next) {
+    User.findOne(req.param('id'), function foundUser(err,user) {
+      if(err) return next(err);
+      if(!user) return next();
+      res.redirect('/user/success' + user);
+    });
+  },
 
+  success:function (req,res) {
+    res.view({
+      user:req.param.user
+    })
+  }
 };
